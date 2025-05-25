5538c4f216d4ff23a1bd5fe6fd1365ea323ae0ff
Sudip Parajuli <sudip.parajulee@gmail.com>
2025-05-25 06:52:02 +0545

    auth login

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -8,6 +8,17 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
+    <div class="bg-blue-800 text-white flex justify-end py-1 px-20">
+        @auth
+        <div class="flex items-center gap-2">
+            <span class="text-sm font-bold">Hi, {{auth()->user()->name}}</span>
+        </div>
+        @else
+        <a href="{{route('login')}}" class="text-sm font-bold">Login</a>
+        @endauth
+    </div>
+
+    </div>
     <nav class="shadow px-20 py-2 flex justify-between items-center">
         <img src="https://smc.edu.np/wp-content/uploads/2023/11/smc-logo-circle-150x150.png" alt="" class="h-20">
         <div class="flex gap-4">
@@ -18,7 +29,6 @@
             @foreach($categories as $category)
              <a href="{{route('categoryproducts',$category->id)}}" class="text-gray-600">{{$category->name}}</a>
             @endforeach
-            <a href="{{route('login')}}" class="text-gray-600">Login</a>
         </div>
     </nav>
     @yield('content')
